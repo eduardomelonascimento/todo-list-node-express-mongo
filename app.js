e16e232cf454809f6cac358c116797dc15c7fb09
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-06-07 14:26:04 -0300

    updates

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use('/checklists', checkListRouter);
 app.use('/checklists', taskRouter.checklistDependent);
 app.use('/tasks', taskRouter.simpleRouter);
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log('Server started');
 });
